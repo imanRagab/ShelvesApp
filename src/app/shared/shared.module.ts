f168525b6ccd570b3961cce16cbf7502b96d0e00
Iman Ragab <iman.mansour94@gmail.com>
2018-06-05 21:55:46 +0200

    add api service

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpTokenInterceptor } from './interceptors/http-token-interceptor';
 
+import {
+    JwtService,
+    ApiService
+} from './services';
+
 @NgModule({
   imports: [
     CommonModule,
   ],
   declarations: [],
   providers: [
-    
+    JwtService,
+    ApiService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpTokenInterceptor,
