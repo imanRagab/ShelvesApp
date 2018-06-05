627687a3d7337ef3271ac0de161c33a81ad6dda0
Iman Ragab <iman.mansour94@gmail.com>
2018-06-05 19:52:28 +0200

    add book model

---
+++ src/app/shared/models/book.model.ts
@@ -0,0 +1,14 @@
+import { User } from './user.model';
+import { Category } from './category.model';
+
+export interface Book {
+    name: string;
+    description: string;
+    rate: number;
+    price: number;
+    quantity: number
+    transaction: number;
+    category: Category
+    user: User;
+    bid_user: User;
+}
