47c07fa1f7f733ae3070b7e3a53bb1c449105ffc
Iman Ragab <iman.mansour94@gmail.com>
2018-06-10 00:10:02 +0200

    add edit profile form

--- src/app/profile/edit-profile/edit-profile.component.ts
+++ src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
 import {
   User,
   UserService,
@@ -13,18 +15,45 @@ export class EditProfileComponent implements OnInit {
 
   currentUser: User;
   userRole: string;
+  editForm: FormGroup;
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
-        // Load the current user's data
-        this.userService.currentUser.subscribe(
-          (userData: User) => {
-            this.currentUser = userData;
-            this.userRole = this.currentUser.role;
-          }
-        );  
+    this.editForm = this.fb.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'name': ['', Validators.required],
+      'profile_picture': ['', Validators.required]
+    });
+    this.loadCurrentUser();
   }
 
+  // load the current user's data
+  loadCurrentUser() {
+    this.userService.currentUser.subscribe(
+      (userData: User) => {
+        if(userData.name){
+          this.currentUser = userData;
+          this.userRole = this.currentUser.role;
+          if(this.userRole == 'Normal user'){
+            this.editForm.addControl('gender', new FormControl('',Validators.required));
+          }
+        }
+      }
+    );  
+  }
+
+  // update user 
+  updateUser() {
+    this.userService.update(this.editForm.value).subscribe(
+      result => {
+        this.currentUser = result;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
