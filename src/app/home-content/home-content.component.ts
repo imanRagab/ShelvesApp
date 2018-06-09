9673353f50cd5d5b19cbd708aacfa037f68cf1f1
Iman Ragab <iman.mansour94@gmail.com>
2018-06-09 21:04:25 +0200

    add recommended books to home

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -15,14 +15,17 @@ import {
 export class HomeContentComponent implements OnInit {
 
   latestBooks: Array<Book>;
+  recommendedBooks: Array<Book>;
   constructor(
     private bookService: BookService
   ) { 
-
   }
 
   ngOnInit() {
+    this.recommendedBooks = [];
+    this.latestBooks = [];
     this.getLatestBooks();
+    this.getRecommendedBooks();
   }
 
   // get latest books list
@@ -30,14 +33,39 @@ export class HomeContentComponent implements OnInit {
 
     this.bookService.getLatest().subscribe(
       result => {
-        console.log(result)
-        result = result['books'];
-        console.log(result[0]['book_images'][0]['image'].url)
-        this.latestBooks = result;
+        this.latestBooks = result['books'];
       },
       error => {
         console.log(error);
       }
     );
   }
+
+  // get recommended books
+  getRecommendedBooks() {
+    this.bookService.getRecommended().subscribe(
+      result => {
+        for(let j = 0; j < result['books'].length; j++) {
+          for(let i = 0; i < result['books'][j].length; i++){
+            this.recommendedBooks.push(result['books'][j][i]);
+          }
+        }
+        this.shuffleRecommendedBooks();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+    // shuffle recommended books --- using Fisher–Yates shuffle algorithm
+    shuffleRecommendedBooks() {
+      var m = this.recommendedBooks.length, temp, i;    
+      while (m) {
+        i = Math.floor(Math.random() * m--);
+        temp = this.recommendedBooks[m];
+        this.recommendedBooks[m] = this.recommendedBooks[i];
+        this.recommendedBooks[i] = temp;
+      }    
+    }
 }
