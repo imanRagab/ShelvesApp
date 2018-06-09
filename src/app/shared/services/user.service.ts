9168b54b5d80f02367d4384371a7719cfb318b6f
Iman Ragab <iman.mansour94@gmail.com>
2018-06-10 01:59:27 +0200

    change get user books url

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -99,7 +99,7 @@ export class UserService {
 
   // get user books
   getUserBooks(id: number): Observable<Array<Book>>{
-    const route = `/user/books/${id}`;
+    const route = `/user/users/${id}/get_user_book`;
     return this.apiService.get(route);
   }
 }
