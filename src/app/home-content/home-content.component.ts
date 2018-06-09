d024c340053a83073c18848dd04232ee7544b2af
Iman Ragab <iman.mansour94@gmail.com>
2018-06-09 16:16:31 +0200

    add categories to nav-bar

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  Book,
+  BookService,
+  User,
+  UserService,
+} from '../shared';
+
 @Component({
   selector: 'app-home-content',
   templateUrl: './home-content.component.html',
@@ -7,11 +14,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeContentComponent implements OnInit {
 
-  constructor() { 
-    
+  firstLatestBooks: Array<Book>;
+  secondLatestBooks: Array<Book>;
+  thirdLatestBooks: Array<Book>;
+  constructor(
+    private bookService: BookService
+  ) { 
+
   }
 
   ngOnInit() {
   }
 
+  // get latest books list
+  getLatestBooks() {
+
+    this.bookService.getLatest().subscribe(
+      result => {
+        result = result['data'];
+          for(let i = 0; i < result.length; i++){
+            if(i <= result.length/3)
+              {
+                this.firstLatestBooks.push(result[i]);
+              }
+            else if(i <= result.length*2/3)
+              {
+
+                this.secondLatestBooks.push(result[i]);
+              }
+            else
+              {
+                this.thirdLatestBooks.push(result[i]);
+              }
+          }
+      },
+      error => {
+
+      }
+    );
+  }
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Category,
+  CategoryService
+} from '../shared';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  firstCatArray: Array<Category>;
+  secondCatArray: Array<Category>;
+  thirdCatArray: Array<Category>;
+  constructor(
+    private categoryService: CategoryService,
+  ) {
+    this.firstCatArray = [];
+    this.secondCatArray = []; 
+    this.thirdCatArray = [];
+   }
 
   ngOnInit() {
+        // get list of book categories
+        this.getCategories();
   }
+    // get list of all categories
+    getCategories() {
+      this.categoryService.getCategories().subscribe(
+        result => {
+          result = result['data'];
+          for(let i = 0; i < result.length; i++){
+            if(i <= result.length/3)
+              {
+                this.firstCatArray.push(result[i]);
+              }
+            else if(i <= result.length*2/3)
+              {
+
+                this.secondCatArray.push(result[i]);
+              }
+            else
+              {
+                this.thirdCatArray.push(result[i]);
+              }
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
 
 }
