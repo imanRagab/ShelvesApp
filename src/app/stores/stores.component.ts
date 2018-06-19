fcdf666f3b8a16e7bf0adc080ce1f8023620b306
Iman Ragab <iman.mansour94@gmail.com>
2018-06-19 17:32:13 +0200

    add pagination to stores

--- src/app/stores/stores.component.ts
+++ src/app/stores/stores.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import {
 
   User,
@@ -12,11 +14,30 @@ import {
   styleUrls: ['./stores.component.scss']
 })
 export class StoresComponent implements OnInit {
+
   bookStores: Array<User>;
   bookStoreImage: any;
-  constructor(private userService: UserService) { }
+  paginationMeta: object;
+  currentPage: number;
+  pages: Array<number>;
+  totalPages: number;
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+
+  ) { 
+    this.route.queryParams.subscribe(params => {        
+      if(params['page'])
+        this.currentPage = parseInt(params['page']);
+      else
+        this.currentPage = 1;
+    });
+  }
 
   ngOnInit() {
+    this.paginationMeta = {};
+    this.totalPages = 0;
     this.bookStoreImage = `${environment.api_host}`;
     this.getAllBookStores();
   }
@@ -26,16 +47,22 @@ export class StoresComponent implements OnInit {
       this.userService.getBookStores().subscribe(
         result => {
           if (result['status'] != 'FAIL') {
-            this.bookStores = result['users'];
-            console.log(this.bookStores);
-            console.log(this.bookStoreImage);
-  
+            this.bookStores = result['users']; 
+            this.paginationMeta = result['meta']['pagination']
+            this.totalPages = this.paginationMeta['total_pages'];
+            this.pages = Array(this.totalPages).fill(0, 0, this.totalPages).map((x,i)=>i);
           }
         },
         error => {
           console.log(error);
         }
       );
+    }
+
+    pageClick(page) {
+
+      let paramsUrl = `?page=${page}`;
+      this.router.navigateByUrl('/stores' +  paramsUrl);
     }
 
 }
